549657088ead90e5190e094b369e64d1df02e0bc
KhotSaprem <141000523+KhotSaprem@users.noreply.github.com>
2024-09-02 23:20:33 +0530

    Update firebase.js to remove the API data

--- Final SGU HACKERA Submission/myReactApp/src/firebase.js
+++ Final SGU HACKERA Submission/myReactApp/src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjiJP1lAfswSpFpdPeabedhmtDFfAukZU",
-  authDomain: "project-management-tool-ea31f.firebaseapp.com",
-  projectId: "project-management-tool-ea31f",
-  storageBucket: "project-management-tool-ea31f.appspot.com",
-  messagingSenderId: "934966707074",
-  appId: "1:934966707074:web:0487920d07f5810092c59b",
-  measurementId: "G-DCJ00QSRPR"
+  //Firebase data here
 };
 
 const app = initializeApp(firebaseConfig);
